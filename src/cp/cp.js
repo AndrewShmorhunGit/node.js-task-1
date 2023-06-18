3e53828622370c284f905fb2b14f9ae0354ffbcd
Andrew Shmorhun <andrewshmorgun@gmail.com>
2023-06-18 23:25:47 +0300

    add workers fn

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { spawn } from "child_process";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export async function spawnChildProcess(args) {
+  let initiated = false;
+
+  const childProcess = spawn("node", [
+    `${dirname}/files/script.js`,
+    ...args.split(""),
+  ]);
+  process.stdin.on("data", (data) => {
+    console.log(data.toString());
+    if (!initiated) {
+      console.log("Write to the console.." + "\n");
+      initiated = true;
+    }
+  });
+}
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess("--silent --all");
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-await performCalculations();
+export async function performCalculations() {
+  const cp = cpus();
+  console.log(cp.length);
+  let num = 10;
+
+  const workersResults = await Promise.allSettled(
+    cp.map(() => {
+      return new Promise((res, rej) => {
+        const worker = new Worker(__dirname + "/worker.js", {
+          workerData: num++,
+        });
+        worker.on("message", (msg) => res(msg));
+        worker.on("error", (msg) => rej(msg));
+      });
+    })
+  );
+
+  const results = workersResults.map(({ status, value }) => ({
+    status: status === "fulfilled" ? "resolved" : "error",
+    data: status === "fulfilled" ? value : null,
+  }));
+
+  console.log(results);
+
+  return results;
+}
+
+performCalculations();
