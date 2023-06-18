c05f69c9b8b972d5e59b0635fe9bb66ed317517d
Andrew Shmorhun <andrewshmorgun@gmail.com>
2023-06-18 16:22:09 +0300

    add zip fn

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
-const compress = async () => {
-    // Write your code here 
-};
+import { createGzip } from "zlib";
+import { createReadStream, createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-await compress();
+export async function compress() {
+  const readStream = createReadStream(__dirname + "/files/fileToCompress.txt");
+  const writeStream = createWriteStream(__dirname + "/files/archive.gz");
+  const toGz = createGzip();
+
+  readStream.pipe(toGz).pipe(writeStream);
+
+  console.log("fileToCompress.txt was archived!");
+}
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
-const decompress = async () => {
-    // Write your code here 
-};
+import { createUnzip } from "zlib";
+import { createReadStream, createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-await decompress();
+export async function decompress() {
+  const writeStream = createWriteStream(
+    __dirname + "/files/fileToCompress.txt"
+  );
+  const readStream = createReadStream(__dirname + "/files/archive.gz");
+  const toGz = createUnzip();
+
+  readStream.pipe(toGz).pipe(writeStream);
+
+  console.log("file archive.gz was extracted to files directory!");
+}
+decompress();
