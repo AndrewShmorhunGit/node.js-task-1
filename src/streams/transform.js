43fdd53e83698d0fc489f3d09213aeb8be39ec2e
Andrew Shmorhun <andrewshmorgun@gmail.com>
2023-06-18 15:54:17 +0300

    add streams fn

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
-const transform = async () => {
-    // Write your code here 
-};
+import { Transform, pipeline } from "stream";
+import { EOL } from "os";
+import { error } from "console";
 
-await transform();
+export async function transform() {
+  const revert = new Transform({
+    transform(chunk, encoding, callback) {
+      const result = chunk.toString();
+      const reverse =
+        result.replace(EOL, "").split("").reverse().join("") + EOL;
+      callback(null, reverse);
+    },
+  });
+
+  pipeline(process.stdin, revert, process.stdout, (err) =>
+    console.error(error)
+  );
+  console.log("Write some to transform it! Please");
+}
+
+transform();
